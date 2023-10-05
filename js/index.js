77fe97b9d1088f789a7be22bff65b37efb019235
César Filipe António <cesarfilipeantonio2404@gmail.com>
2023-10-05 14:26:44 +0100

    fiz a pesquisa e coloquei etilos nos card

--- js/index.js
+++ js/index.js
@@ -1,73 +1,107 @@
-// const array=[
-//   {nome:"césar0", id:0},
-//   {nome:"césar1", id:1,},
-//   {nome:"césar2", id:2,},
-//   {nome:"césar3", id:3,},
-//   {nome:"césar4", id:4,},
-//   {nome:"césar5", id:5,},
-//   {nome:"césar6", id:6,},
-//   {nome:"césar7", id:7,},
-//   {nome:"césar8", id:8,}
-// ]
+const array=[
+  {nome:"césar0", id:0},
+  {nome:"Domingos", id:1,},
+  {nome:"André", id:2,},
+  {nome:"Pessela", id:3,},
+  {nome:"Adão", id:4,},
+  {nome:"Francisco", id:5,},
+  {nome:"sar6", id:6,},
+  {nome:"Telma", id:7,},
+  {nome:"Mira", id:8,}
+]
+let array2 = array;
 let ul = document.querySelector(`ul`);
 const ce = (el)=>document.createElement(el);
-var minhaPronise = function() {
-  return new Promise(function(resolve, reject){
-    var xhr = new XMLHttpRequest();
 
-    xhr.open('get', 'https://api.github.com/users')
-    xhr.send(null)
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState === 4){ 
-        if(xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText))     
-         } else {
-        reject(`Dados Não Encontrados!`)
-     }
-    }
-    }
-   })  
+
+function renderElmento(){
+  ul.innerHTML=""
+  array2.map((element)=>{
+    let li = ce("li");
+    let img =ce("img");
+    img.setAttribute(`src`,`${element.avatar_url}`)
+    let sectionNome = ce("section");
+    let sectionId = ce("section");
+    sectionNome.innerHTML=`Nome: <h1>${element.nome}</h1>`;
+    sectionId.innerHTML=`Id: <h1>${element.id}</h1>`
+    li.appendChild(img)
+    li.appendChild(sectionId)
+    li.appendChild(sectionNome);
+    ul.appendChild(li)
+  });
 }
+renderElmento()
 
-minhaPronise()
-.then(function(response){
-
-  let array= response;
-  console.log(array)
-
-  function renderElmento(){
-    array.map((element)=>{
-      let li = ce("li");
-      let img =ce("img");
-      img.setAttribute(`src`,`${element.avatar_url}`)
-      let sectionNome = ce("section");
-      let sectionId = ce("section");
-      sectionNome.innerHTML=`Nome: <h1>${element.login}</h1>`;
-      sectionId.innerHTML=`Id: <h1>${element.id}</h1>`
-      li.appendChild(img)
-      li.appendChild(sectionId)
-      li.appendChild(sectionNome);
-      ul.appendChild(li)
-    });
-  }
-  renderElmento()
-  
   /*area pesquisa*/
   inputPesquisa.addEventListener(`keyup`, (event) => {
     const search = event.target.value;
-    const retorneArray = response.filter((element) => element.nome.toLowerCase().includes(search.toLowerCase()));
+    const retorneArray = array.filter((element) => element.nome.toLowerCase().includes(search.toLowerCase()));
     if (search == "") {
-        array = response;
+        array2 = array;
     } else {
-        array = retorneArray;
+        array2 = retorneArray;
     }
 
   renderElmento()
 });
-})
-.catch(function(error){
-  console.log(error)
-  let tratarErro = document.querySelector(`.card`);
-  tratarErro.innerHTML=`<h1 id ="tratarErro">${error}</h1>`;
-})
+
+// var minhaPronise = function() {
+//   return new Promise(function(resolve, reject){
+//     var xhr = new XMLHttpRequest();
+
+//     xhr.open('get', 'https://api.github.com/users')
+//     xhr.send(null)
+//     xhr.onreadystatechange = function() {
+//       if(xhr.readyState === 4){ 
+//         if(xhr.status === 200) {
+//           resolve(JSON.parse(xhr.responseText))     
+//          } else {
+//         reject(`Dados Não Encontrados!`)
+//      }
+//     }
+//     }
+//    })  
+// }
+
+// minhaPronise()
+// .then(function(response){
+
+//   let array= response;
+//   console.log(array)
+
+  // function renderElmento(){
+  //   array.map((element)=>{
+  //     let li = ce("li");
+  //     let img =ce("img");
+  //     img.setAttribute(`src`,`${element.avatar_url}`)
+  //     let sectionNome = ce("section");
+  //     let sectionId = ce("section");
+  //     sectionNome.innerHTML=`Nome: <h1>${element.login}</h1>`;
+  //     sectionId.innerHTML=`Id: <h1>${element.id}</h1>`
+  //     li.appendChild(img)
+  //     li.appendChild(sectionId)
+  //     li.appendChild(sectionNome);
+  //     ul.appendChild(li)
+  //   });
+  // }
+  // renderElmento()
+  
+  /*area pesquisa*/
+//   inputPesquisa.addEventListener(`keyup`, (event) => {
+//     const search = event.target.value;
+//     const retorneArray = response.filter((element) => element.nome.toLowerCase().includes(search.toLowerCase()));
+//     if (search == "") {
+//         array = response;
+//     } else {
+//         array = retorneArray;
+//     }
+
+//   renderElmento()
+// });
+// })
+// .catch(function(error){
+//   console.log(error)
+//   let tratarErro = document.querySelector(`.card`);
+//   tratarErro.innerHTML=`<h1 id ="tratarErro">${error}</h1>`;
+// })
 

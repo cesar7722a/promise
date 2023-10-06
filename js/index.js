b2ce1494d9906f78508530e2d1f444fa74983c42
César Filipe António <cesarfilipeantonio2404@gmail.com>
2023-10-06 21:57:31 +0100

    removi o array de exemplo

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
-// const array=[
-//   {nome:"césar0", id:0},
-//   {nome:"Domingos", id:1,},
-//   {nome:"André", id:2,},
-//   {nome:"Pessela", id:3,},
-//   {nome:"Adão", id:4,},
-//   {nome:"Francisco", id:5,},
-//   {nome:"sar6", id:6,},
-//   {nome:"Telma", id:7,},
-//   {nome:"Mira", id:8,}
-// ]
-// let array2 = array;
+
 const userCadastrados = JSON.parse(localStorage.getItem(`user`)) || [];
 let ul = document.querySelector(`ul`);
 const ce = (el)=>document.createElement(el);
@@ -32,6 +21,21 @@ var minhaPronise = function() {
    })  
 }
 
+function Cancel() {
+  document.querySelector('.modal').style.opacity = 1;
+  setTimeout(() => {
+    document.querySelector(`.modal`).style.display = "none";
+    document.querySelector('.modal').style.opacity = 0;
+  }, 200);
+  document.querySelector(`#inputEmail`).value = "";
+  document.querySelector(`#inputPassword`).value = "";
+};
+   
+function criarConta () {
+  window.location.replace("pageCriarConta.html");
+}
+
+
  minhaPronise()
  .then(function(response){
 
@@ -49,12 +53,11 @@ var minhaPronise = function() {
        img.setAttribute(`src`,`${element.avatar_url}`)
        let sectionNome = ce("section");
        let sectionId = ce("section");
-       sectionNome.innerHTML=`Nome: <h1>${element.nome}</h1>`;
+       sectionNome.innerHTML=`Nome: <h1>${element.login}</h1>`;
        sectionId.innerHTML=`Id: <h1>${element.id}</h1>`
    
        li.addEventListener(`click`, ()=>{
-         mostraMoodal ()
-         savePosicao() 
+         mostraMoodal ();
        })
        li.appendChild(img)
        li.appendChild(sectionId)
@@ -62,12 +65,12 @@ var minhaPronise = function() {
        ul.appendChild(li)
      });
    }
-   renderElmento()
+   renderElmento();
    
      /*area pesquisa*/
      inputPesquisa.addEventListener(`keyup`, (event) => {
        const search = event.target.value;
-       const retorneArray = array.filter((element) => element.nome.toLowerCase().includes(search.toLowerCase()));
+       const retorneArray = array.filter((element) => element.login.toLowerCase().includes(search.toLowerCase()));
        if (search == "" || search.trim()=="") {
            array = response;
        } else {
@@ -76,7 +79,7 @@ var minhaPronise = function() {
          //  tratarErro.innerHTML=`<h1 id ="tratarErro">Nome Não Encontrado!</h1>`;
        }
    
-     renderElmento()
+     renderElmento();
    });
    
    // Modal
@@ -87,23 +90,8 @@ var minhaPronise = function() {
      setTimeout(() => {
        document.querySelector('.modal').style.opacity = 1;
      }, 200);
-     console.log(posicao)
-     console.log(posicao)
    }
-   
-   function Cancel() {
-     document.querySelector('.modal').style.opacity = 1;
-     setTimeout(() => {
-       document.querySelector(`.modal`).style.display = "none";
-       document.querySelector('.modal').style.opacity = 0;
-     }, 200);
-     document.querySelector(`#inputEmail`).value = "";
-     document.querySelector(`#inputPassword`).value = "";
-   };
-   
-   function criarConta () {
-     window.location.replace("pageCriarConta.html");
-   }
+
    document.querySelector(`.btn-sign-up`).addEventListener(`click`,()=>{
      let valorEmali =  document.querySelector(`#inputEmail`).value;
      let valorPassword = document.querySelector(`#inputPassword`).value;
@@ -113,13 +101,10 @@ var minhaPronise = function() {
        if(verifacar_Emai_Existencia > 0 && verifacar_password_Existencia > 0 ){
          window.location.replace("perfilUser.html");
        }else{
-         alert("Dados incorretos")
+         alert("Dados incorretos");
        }
    });
-   
-   function savePosicao() {
-     localStorage.setItem(`posição`, JSON.stringify(posicao));
-   }
+
 
 
 

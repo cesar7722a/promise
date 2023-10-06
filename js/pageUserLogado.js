d9c0c84518715fdd0e56409b512aac8c8cb1bfa6
César Filipe António <cesarfilipeantonio2404@gmail.com>
2023-10-06 22:19:21 +0100

    trabalhei na pesquisa

--- js/pageUserLogado.js
+++ js/pageUserLogado.js
@@ -3,6 +3,23 @@ let ul = document.querySelector(`ul`);
 const ce = (el)=>document.createElement(el);
 
 
+// var minhaPronise = function() {
+//   return new Promise(function(resolve, reject){
+//     var xhr = new XMLHttpRequest();
+
+//     xhr.open('get', 'https://api.github.com/users')
+//     xhr.send(null)
+//     xhr.onreadystatechange = function() {
+//       if(xhr.readyState === 4){ 
+//         if(xhr.status === 200) {
+//           resolve(JSON.parse(xhr.responseText))     
+//          } else {
+//         reject(`Dados Não Encontrados!`)
+//      }
+//     }
+//     }
+//    })  
+// }
 var minhaPronise = function() {
   return new Promise(function(resolve, reject){
     var xhr = new XMLHttpRequest();
@@ -35,7 +52,7 @@ function renderElmento(){
     img.setAttribute(`src`,`${element.avatar_url}`)
     let sectionNome = ce("section");
     let sectionId = ce("section");
-    sectionNome.innerHTML=`Nome: <h1>${element.nome}</h1>`;
+    sectionNome.innerHTML=`Nome: <h1>${element.login}</h1>`;
     sectionId.innerHTML=`Id: <h1>${element.id}</h1>`
 
     li.addEventListener(`click`, ()=>{window.location.replace(`perfilUser.html`)});
@@ -50,7 +67,7 @@ renderElmento()
   /*area pesquisa*/
   inputPesquisa.addEventListener(`keyup`, (event) => {
     const search = event.target.value;
-    const retorneArray = response.filter((element) => element.nome.toLowerCase().includes(search.toLowerCase()));
+    const retorneArray = response.filter((element) => element.login.toLowerCase().includes(search.toLowerCase()));
     if (search == "" || search.trim()=="") {
         array = response;
     } else {

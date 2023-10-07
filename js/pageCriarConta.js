ba744526eca68311a207415d4b64af8b79abd23c
César Filipe António <cesarfilipeantonio2404@gmail.com>
2023-10-07 23:29:46 +0100

    valição de user js

--- js/pageCriarConta.js
+++ js/pageCriarConta.js
@@ -20,6 +20,34 @@ function adicionarElemento(){
 document.querySelector(`.btn-voltar`).addEventListener(`click`,()=>{
   window.location.replace("index.html");
 });
+
+function validarUser(){
+  let novoNome = document.querySelector(`#inputNome`).value;
+  var resultNome = validarNome(novoNome);
+  if(resultNome) {
+    document.querySelector(`#inputNome`).setAttribute(`class`,`userDadosCorretos`);
+  } else {
+    document.querySelector(`#inputNome`).setAttribute(`class`,`userDadosErrado`);
+  }
+}
+
+function validarUserPass(){
+  let novaPassword = document.querySelector(`#inputPassword`).value;
+  var resultPassword = validarPassword(novaPassword)
+   if(resultPassword) {
+    document.querySelector(`#inputPassword`).setAttribute(`class`,`userDadosCorretos`);                    
+   } else{
+    document.querySelector(`#inputPassword`).setAttribute(`class`,`userDadosErrado`);
+   }
+}
+
+function validarNome(nome){
+return /^[a-zA-Z]{5,}$/.test(nome);
+}
+function validarPassword(password){
+  return /^[a-zA-Z0-9]{6,}$/.test(password);
+}
+
 function saveUserCadastrado() {
   localStorage.setItem(`user`, JSON.stringify(userCadastrados));
 }
